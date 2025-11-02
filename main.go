1947d62ba673a97bf7d266480264293eb182db58
k-kimtaI <tezkim414@gmail.com>
2025-11-02 16:30:16 +0300

    adding  new blocks

--- main.go
+++ main.go
@@ -9,10 +9,14 @@ import (
 )
 
 type Block struct {
-	timestamp  		int64
-	data 			[]byte
-	prevBlockHash 	[]byte
-	hash 			[]byte
+	Timestamp  		int64
+	Data 			[]byte
+	PrevBlockHash 	[]byte
+	Hash 			[]byte
+}
+
+type Blockchain struct {
+	blocks []*Block
 }
 
 // Calculate the hash of the block
@@ -20,15 +24,15 @@ func (b *Block) setHash() {
 	headers := bytes.Join(
 		// timestamp, data, prevhash
 		[][]byte{
-			IntToHex(b.timestamp),
-			b.data,
-			b.prevBlockHash,
+			IntToHex(b.Timestamp),
+			b.Data,
+			b.PrevBlockHash,
 		},
 		[]byte{},
 	)
 	// convert headers to hash
 	hash := sha256.Sum256(headers)
-	b.hash = hash[:]
+	b.Hash = hash[:]
 }
 
 // create function IntToHex to convert int64 to byte array
@@ -38,15 +42,41 @@ func IntToHex( x int64) []byte {
 	return buf.Bytes()
 }
 
+// function to create a new block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.setHash()
+	return block
+}
+
+//add blocks to blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// first block in the chain -> genesis block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+// funct to create a blockchain with the genesis block
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
 // main function
 func main() {
-	b := &Block{
-		timestamp: time.Now().Unix(),
-		data: []byte("Hello, blockchain!"),
-		prevBlockHash: []byte{},
+	bc := NewBlockchain()
+	bc.AddBlock("Send 0.1 BTC to jon")
+	bc.AddBlock("Send 0.2 BTC to mike")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("hash:: %x\n", block.Hash)
+		fmt.Println()
 	}
 
-	b.setHash()
-	// %x to print byte array in hex format
-	fmt.Printf("Blockhash: %x \n", b.hash)
 }
