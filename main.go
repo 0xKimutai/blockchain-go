a6517882937d452318dac4f1c5c3c6f0b879f7fd
k-kimtaI <tezkim414@gmail.com>
2025-11-02 14:35:26 +0300

    first block, print blockhash

--- main.go
+++ main.go
@@ -9,15 +9,16 @@ import (
 )
 
 type Block struct {
-	timestamp     int64
-	data          []byte
-	prevBlockHash []byte
-	hash          []byte
+	timestamp  		int64
+	data 			[]byte
+	prevBlockHash 	[]byte
+	hash 			[]byte
 }
 
-// SetHash calculates and sets the hash of the block.
-func (b *Block) SetHash() {
+// Calculate the hash of the block
+func (b *Block) setHash() {
 	headers := bytes.Join(
+		// timestamp, data, prevhash
 		[][]byte{
 			IntToHex(b.timestamp),
 			b.data,
@@ -25,24 +26,27 @@ func (b *Block) SetHash() {
 		},
 		[]byte{},
 	)
+	// convert headers to hash
 	hash := sha256.Sum256(headers)
 	b.hash = hash[:]
 }
 
-// IntToHex converts an int64 to a byte slice.
-func IntToHex(n int64) []byte {
+// create function IntToHex to convert int64 to byte array
+func IntToHex( x int64) []byte {
 	buf := new(bytes.Buffer)
-	_ = binary.Write(buf, binary.BigEndian, n)
+	_ = binary.Write(buf, binary.BigEndian, x)
 	return buf.Bytes()
 }
 
-// Example usage
+// main function
 func main() {
 	b := &Block{
-		timestamp:     time.Now().Unix(),
-		data:          []byte("Hello, blockchain"),
+		timestamp: time.Now().Unix(),
+		data: []byte("Hello, blockchain!"),
 		prevBlockHash: []byte{},
 	}
-	b.SetHash()
-	fmt.Printf("Block hash: %x\n", b.hash)
+
+	b.setHash()
+	// %x to print byte array in hex format
+	fmt.Printf("Blockhash: %x \n", b.hash)
 }
